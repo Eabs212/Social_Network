0a531d937e2f0d26e371c9ca15d79ce1c991c9c3
Rafael Romero <rafajrg21@gmail.com>
2019-04-28 17:49:42 -0400

    Frontend Refactor
    Se le hicieron algunos cambios a los estilos de la pagina principal, el login y el signup

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -30,7 +30,7 @@ fetch("./../register", params)
 }
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems,{ format:"yyyy-mm-dd",yearRange:[1910,2019]});
+    var instances = M.Datepicker.init(elems,{ format:"yyyy-mm-dd",yearRange:[1970,2019]});
   });
 
 
